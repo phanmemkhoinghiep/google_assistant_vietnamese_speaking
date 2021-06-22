4e7a8ecdda38d09acf6267b78c52e4960499865d
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2021-06-22 07:49:36 +0700

    Add files via upload

--- src/create_config.py
+++ src/create_config.py
@@ -18,6 +18,52 @@ data['mic'].append({
     'type': 'ReSpeaker Core v2.0',
     'is_active': False        
 })    
+data['volume'] = []
+data['volume'].append({
+    'value': 50,
+    'type': 'speak'    
+})
+data['volume'].append({
+    'value': 50,
+    'type': 'event'    
+})
+data['hotword'] = []
+data['hotword'].append({
+    'name': 'hey siri',
+    'keyword_path': 'hey siri_raspberry-pi.ppn',    
+    'sensitive': 0.3,        
+    'is_active': True    
+})
+data['hotword'].append({
+    'name': 'americano',
+    'keyword_path': 'americano_raspberry-pi.ppn',    
+    'sensitive': 0.3,        
+    'is_active': True    
+})
+data['hotword'].append({
+    'name': 'blueberry',
+    'keyword_path': 'blueberry_raspberry-pi.ppn',    
+    'sensitive': 0.3,        
+    'is_active': True    
+})
+data['hotword'].append({
+    'name': 'terminator',
+    'keyword_path': 'terminator_raspberry-pi.ppn',    
+    'sensitive': 0.3,        
+    'is_active': False    
+})
+data['hotword'].append({
+    'name': 'ok google',
+    'keyword_path': 'ok google_raspberry-pi.ppn',    
+    'sensitive': 0.3,        
+    'is_active': True    
+})
+data['hotword'].append({
+    'name': 'hey google',
+    'keyword_path': 'hey google_raspberry-pi.ppn',    
+    'sensitive': 0.3,        
+    'is_active': True   
+})
 
 with open('config.json', 'w') as outfile:
     json.dump(data, outfile)
